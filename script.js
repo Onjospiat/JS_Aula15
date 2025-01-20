6e07f2709e04747a8cab5076bccb37f2d6e3427a
Onjospiat <guilherme.chini.gani@gmail.com>
2025-01-20 16:44:23 -0300

    Final da aula 15

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function changeText(){
-    var x = document.getElementsByTagName("h1");
-    x[0].innerHTML = "Hello!";
+    var pt = document.getElementsByClassName("pt-BR");
+    var en = document.getElementsByClassName("en");
+    var i=0;
+    if(pt[0].style.display == "none"){
+        for(i=0;i<pt.length;i++){
+            pt[i].style.display = "block";
+        }
+        i=0;
+        for(i=0;i<en.length;i++){
+            en[i].style.display = "none";
+        }
+    }
+    else{
+        for(i=0;i<pt.length;i++){
+            pt[i].style.display = "none";
+        }
+        i=0;
+        for(i=0;i<en.length;i++){
+            en[i].style.display = "block";
+        }
+    }
+    
 }
 
 window.changeText = changeText;
