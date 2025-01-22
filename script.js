3f1531d2cd671a82d12e7068d3c60bfa27b350ca
Onjospiat <guilherme.chini.gani@gmail.com>
2025-01-22 16:05:20 -0300

    Comentários corrigidos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var language = localStorage.getItem('language',language) || "pt-BR"; //Guarda variável offline como cookie
+var language = localStorage.getItem('language',language) || "pt-BR"; //Pega a variável offline como cookie
 
 function switchLang(){ //Função no botão que muda o idioma
     if(language == "pt-BR"){
@@ -16,7 +16,7 @@ function changeText(){ //Essa função esconde o texto em um idioma e exibe o de
     let en = document.getElementsByClassName("en"); //Cria uma lista com todos os elementos com classe='en'
     let i=0;
 
-    localStorage.setItem('language', language); //Pega o idioma definido (pt-BR || en)
+    localStorage.setItem('language', language); //Define o idioma (pt-BR || en)
 
     if(language == "pt-BR"){ //Exibe os itens da class pt-BR
         for(i=0;i<pt.length;i++){
