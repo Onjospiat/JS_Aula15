c6efd44f2c92e99fb80d1283e95077190fa38481
Onjospiat <guilherme.chini.gani@gmail.com>
2025-01-22 16:02:52 -0300

    Aula 15 com comentários

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-var language = localStorage.getItem('language',language) || "pt-BR";
+var language = localStorage.getItem('language',language) || "pt-BR"; //Guarda variável offline como cookie
 
-function switchLang(){
+function switchLang(){ //Função no botão que muda o idioma
     if(language == "pt-BR"){
         language = "en";
     }
     else{
         language = "pt-BR";
     }
-    changeText();
+    changeText(); //chama a função que muda o texto
 }
 
-function changeText(){
-    let pt = document.getElementsByClassName("pt-BR");
-    let en = document.getElementsByClassName("en");
+function changeText(){ //Essa função esconde o texto em um idioma e exibe o de outro idioma
+    // let é usado para variáveis locais
+    let pt = document.getElementsByClassName("pt-BR"); // Cria uma lista com todos os elementos com classe='pt-BR'
+    let en = document.getElementsByClassName("en"); //Cria uma lista com todos os elementos com classe='en'
     let i=0;
 
-    localStorage.setItem('language', language);
+    localStorage.setItem('language', language); //Pega o idioma definido (pt-BR || en)
 
-    if(language == "pt-BR"){
+    if(language == "pt-BR"){ //Exibe os itens da class pt-BR
         for(i=0;i<pt.length;i++){
             pt[i].style.display = "block";
         }
         i=0;
-        for(i=0;i<en.length;i++){
+        for(i=0;i<en.length;i++){ //Esconde os itens da class en
             en[i].style.display = "none";
         }
     }
     else{
-        for(i=0;i<pt.length;i++){
+        for(i=0;i<pt.length;i++){ // Esconde os itens da class pt-BR
             pt[i].style.display = "none";
         }
         i=0;
-        for(i=0;i<en.length;i++){
+        for(i=0;i<en.length;i++){ //Exibe os itens da class en
             en[i].style.display = "block";
         }
     }
